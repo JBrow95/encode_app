577c8698f513e319c7bb911f9fd5399fa2f3b4d9
JBrow95 <browningjustin95@gmail.com>
2018-07-02 14:30:56 -0400

    test passed

--- encode.rb
+++ encode.rb
@@ -1,3 +1,3 @@
 def alph()
-	a = "test"
+	a = Array("a..z")
 end
--- tdd_test.rb
+++ tdd_test.rb
@@ -3,7 +3,7 @@ require_relative "encode.rb"
 
 class TestEncode < Minitest::Test
 
-	def test_if_string_is_class
+	def test_if_string_is_in_array
 		assert_equal(String, alph().class)
 	end
 
