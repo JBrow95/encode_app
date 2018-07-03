d904faf172be725df17797b125a3271d0845b2b2
JBrow95 <browningjustin95@gmail.com>
2018-07-03 10:57:53 -0400

    test failed, no array in func

--- encode.rb
+++ encode.rb
@@ -1,9 +1,24 @@
-msg = ""
+#msg = ""
 
-def alph(msg)
-	msg.tr('A-Za-z', 'F-ZA-Ef-za-e')
-end
+# def alph(msg)
+# 	msg.tr('A-Za-z', 'F-ZA-Ef-za-e')
+# end
 
-def alph_de(msg)
-	msg.tr('F-ZA-Ef-ze-e', "A-Za-z")
+# def alph_de(msg)
+# 	msg.tr('F-ZA-Ef-ze-e', "A-Za-z")
+# end
+
+# string = ""
+# def caesar_cipher(string, key)
+#     string.downcase.gsub(/[a-z]/) { |m| ((m.ord - 'a'.ord + key) % 26 + 'a'.ord).chr }
+# end
+
+# def cipher_shift(s, n)
+#   letters = [*'a'..'z'] 
+#   s.chars.map {|x| letters.include?(x.downcase) ? (x.ord + n).chr : x}.join
+# end
+str = ""
+def encode(str)
+# alphabet = [*'a'..'z']
+
 end
--- tdd_test.rb
+++ tdd_test.rb
@@ -3,18 +3,27 @@ require_relative "encode.rb"
 
 class TestEncode < Minitest::Test
 
-	def test_if_string_is_in_array
-		msg = ""
-		assert_equal(String, alph(msg).class)
-	end
+	# def test_if_string_is_in_array
+	# 	string = ""
+	# 	assert_equal(String, caesar_cipher(string, 0).class)
+	# end
 
-	def test_if_encrypt_equals_msg
-		msg = ""
-		assert_equal("pnqq", alph("kill"))
-	end
+	# def test_if_encrypt_equals_msg
+	# 	string = ""
+	# 	assert_equal("pnqq", caesar_cipher("kill", 5))
+	# end
 
-	def test_if_msg_is_decrypted
-		msg = ""
-		assert_equal("kill", alph_de("pnqq"))
+	# def test_if_msg_is_decrypted
+	# 	string = ""
+	# 	assert_equal("kill", caesar_cipher("pnqq", -5))
+	# end
+
+	#end test for .tr
+
+	#begin test for other way
+
+	def test_if_array_is_a_class
+		str = ""
+		assert_equal(Array, encode(str).class)
 	end
 end
